711bdf9480004e2c85a72dcd34bde6b201373660
Kathy <kathy.tam@mail.mcgill.ca>
2021-06-23 16:48:20 +0000

    Implemented findKey fn

---
+++ findKey.js
@@ -0,0 +1,29 @@
+const assertEqual = function(actual, expected) {
+  let happy = String.fromCodePoint(0x1F600), mad = String.fromCodePoint(0x1F621);
+  (actual === expected) ? console.log(`${happy}${happy}${happy} Assertion Passed: ${actual} === ${expected}`) : console.log(`${mad}${mad}${mad} Assertion Failed: ${actual} !== ${expected}`);
+};
+
+let findKey = function(object, callback) {
+  for(let key in object) {
+    if(callback(object[key])) { return key }  //can't pass key to callback bc we want to pass the actual value 
+  }
+}
+
+// TEST CODE
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2), "noma");
+
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 0), undefined);
