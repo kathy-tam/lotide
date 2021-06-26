64a0de6f49abbea2f0a07368af752fec08bb91b8
Kathy <kathy.tam@mail.mcgill.ca>
2021-06-26 19:57:11 +0000

    Updated to compare nested arrays

--- eqArrays.js
+++ eqArrays.js
@@ -7,7 +7,9 @@ let eqArrays = function(arr1, arr2) {
   let check = true;
   if(arr1.length === arr2.length) {
     for(let i = 0; i < arr1.length; i++) {
-      if(arr1[i] !== arr2[i]) { return false };
+      if(Array.isArray(arr1[i]) && Array.isArray(arr2[i])) {
+        if(!eqArrays(arr1[i], arr2[i])) { return false }
+      } else if(arr1[i] !== arr2[i]) { return false };
     }
   }else{ 
     check = false; 
@@ -21,3 +23,9 @@ assertEqual(eqArrays([1, 2, 3], [1, 2, 4]), false);
 assertEqual(eqArrays([1, 2, 3], [1, 2]), false);
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
 assertEqual(eqArrays(["1", "2", "3"], []), false);
+
+// TEST CODE: NESTED ARRAYS
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]), true);
+
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]), false);
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]), false);
