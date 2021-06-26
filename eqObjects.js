faab0ef7de5058166fc9945e29387a82e45fa72a
Kathy <kathy.tam@mail.mcgill.ca>
2021-06-26 19:52:00 +0000

    Updated function to compare nested objects

--- eqObjects.js
+++ eqObjects.js
@@ -20,6 +20,8 @@ const eqObjects = function(object1, object2) {
     for(let key of Object.keys(object1)) {
       if(Array.isArray(object1[key]) && Array.isArray(object2[key])) {
         if(!eqArrays(object1[key], object2[key])) { return false }
+      } else if (typeof object1[key] === 'object' && typeof object2[key] === 'object') {
+        if(!eqObjects(object1[key], object2[key])) { return false };
       } else if(object1[key] !== object2[key]) { return false }
     }
   } else { return false }
@@ -40,3 +42,8 @@ assertEqual(eqObjects(cd, dc), true);
 
 const cd2 = { c: "1", d: ["2", 3, 4] };
 assertEqual(eqObjects(cd, cd2), false); 
+
+// TEST CODE: NESTED OBJECTS
+assertEqual(eqObjects({ a: { z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }), true);
+assertEqual(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }), false);
+assertEqual(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: 1, b: 2 }), false);
