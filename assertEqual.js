f471ea2cfe740c41ab9295aedc460f9ae4c2e3ff
Kathy <kathy.tam@mail.mcgill.ca>
2021-06-28 02:26:38 +0000

    Created test folder and moduralized code

--- assertEqual.js
+++ assertEqual.js
@@ -3,8 +3,4 @@ const assertEqual = function(actual, expected) {
   (actual === expected) ? console.log(`${happy}${happy}${happy} Assertion Passed: ${actual} === ${expected}`) : console.log(`${mad}${mad}${mad} Assertion Failed: ${actual} !== ${expected}`);
 };
 
-// TEST CODE
-assertEqual("hi", "hi");
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual(1, 1.1);
+module.exports = assertEqual;
