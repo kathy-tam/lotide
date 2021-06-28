9627e5a1ff83c9a6df69ae4067cb42b5bfda983d
Kathy <kathy.tam@mail.mcgill.ca>
2021-06-28 03:19:20 +0000

    Refactored middleTest to use M & C

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,7 +1,16 @@
-const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert;
 const middle = require('../middle');
 
-// TEST CODE
-assertArraysEqual(middle([1,2]), []);
-assertArraysEqual(middle([1,2,3]), [2]);
-assertArraysEqual(middle([1,2,3,4]), [2,3]);
+describe('#middle', () => {
+  it('returns [] for [1,2]', () => {
+    assert.deepEqual(middle([1,2]), []);
+  });
+
+  it('returns [2] for [1,2,3]', () => {
+    assert.deepEqual(middle([1,2,3]), [2]);
+  });
+
+  it('returns [2,3] for [1,2,3,4]', () => {
+    assert.deepEqual(middle([1,2,3,4]), [2,3]);
+  });
+});
